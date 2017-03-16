472f69bc5747c56964141a7cc4f729485c86835b
zuojianghua <28842136@qq.com>
2017-03-16 13:03:13 +0800

    ding

---
+++ api/ding_demo/corp/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DIR_ROOT', dirname(__FILE__).'/');
+define("OAPI_HOST", "https://oapi.dingtalk.com");
+
+define("CORPID", "dingding");
+define("SECRET", "nt8ukl5a96w488iweujwyz0vzx4sdtz8p649ixpsj8g");
+define("AGENTID", "");//必填，在创建微应用的时候会分配
+
---
+++ api/ding_demo/isv/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DIR_ROOT', dirname(__FILE__).'/');
+define("OAPI_HOST", "https://oapi.dingtalk.com");
+//Suite
+define("CREATE_SUITE_KEY", "suite4xxxxxxxxxxxxxxx");
+define("SUITE_KEY", "suite5ut2khmzqcexbimr");
+define("SUITE_SECRET", "SCV5ImzGszfke8e1B2hwn0ydoEfdqMgGSaIaNyK8Op_UNt74JGNi1KVDSxgaw3l8");
+define("TOKEN", "dingding");
+define("APPID", "1230001");
+define("ENCODING_AES_KEY", "nt8ukl5a96w488iweujwyz0vzx4sdtz8p649ixpsj8g");
